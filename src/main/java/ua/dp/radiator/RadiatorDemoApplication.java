43e772ccd31aaaefc3d6a95ebbcfd937b0a3036d
ivan.drizhiruk <ivan.drizhiruk@gmail.com>
2017-07-15 17:40:46 +0300

    Initial commit. Load last build statuses.

--- src/main/java/ua/dp/radiator/RadiatorDemoApplication.java
+++ src/main/java/ua/dp/radiator/RadiatorDemoApplication.java
@@ -3,9 +3,11 @@ package ua.dp.radiator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableAutoConfiguration
+@EnableScheduling
 public class RadiatorDemoApplication {
 
 	public static void main(String[] args) {
--- src/main/java/ua/dp/radiator/rest/BuildStateReportResource.java
+++ src/main/java/ua/dp/radiator/rest/BuildStateReportResource.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 import ua.dp.radiator.domain.BuildState;
 import ua.dp.radiator.repository.BuildStateRepository;
 
-import java.time.ZonedDateTime;
 import java.util.List;
 
 /**
@@ -28,24 +27,9 @@ public class BuildStateReportResource {
     }
 
     @GetMapping(value = "/report/build-states/last", produces = MediaType.APPLICATION_JSON_VALUE)
-//    public RadiatorProperties getLastBuildStates() {
-//        log.debug("REST request to get last BuildStates");
-//
-//        return radiatorProperties;
-//    }
     public List<BuildState> getLastBuildStates() {
         log.debug("REST request to get last BuildStates");
 
-        BuildState buildState = new BuildState();
-        buildState.setInstancesName("test");
-        buildState.setState("Mega");
-        buildState.setErrorMessage("Oops");
-        buildState.setExtractingDate(ZonedDateTime.now());
-
-        buildStateRepository.save(buildState);
-
-        List<BuildState> lastBuildStates = buildStateRepository.findAll();
-
-        return lastBuildStates;
+        return buildStateRepository.findLastBuildStates();
     }
 }
