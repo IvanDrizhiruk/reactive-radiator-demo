4e48f30c6969f5281446c2e4e15d94bd666d599a
ivan.drizhiruk <ivan.drizhiruk@gmail.com>
2017-07-16 15:03:41 +0300

    Problem with mapping of static-resources has been hacked

--- src/main/java/ua/dp/radiator/RadiatorDemoApplication.java
+++ src/main/java/ua/dp/radiator/RadiatorDemoApplication.java
@@ -3,7 +3,12 @@ package ua.dp.radiator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
 
 @SpringBootApplication
 @EnableAutoConfiguration
@@ -13,4 +18,11 @@ public class RadiatorDemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RadiatorDemoApplication.class, args);
 	}
+
+	@Bean
+	public RouterFunction<ServerResponse> route() {
+		//It is hacked for fix problem with mapping of static-resources
+		return RouterFunctions.resources("/**", new ClassPathResource("static/"));
+	}
+
 }
